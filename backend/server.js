82281fcc7117644cdcf9fce4240eec03733e1fd1
hemantgnandane <hemantgnandane@gmail.com>
2025-04-12 11:57:57 +0530

    resolve server.js error

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import http from 'http';
+import app from './app.js';
+ 
+
+const port = process.env.PORT || 3000;
+
+
+const server = http.createServer(app);
+
+
+
+
+server.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+})
